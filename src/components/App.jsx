46bafd52a1ad1b4e2bdeb63d1fdb0876c9502083
Mahmoud Bakr <mbakr6821@gmail.com>
2022-09-12 10:49:03 +0200

    Initialize the app's components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { Route, Routes } from 'react-router-dom';
-import Movies from './Movies';
+import Movies from './Movies/Movies';
+import Profile from './Profile/Profile';
+import MovieInfo from './MovieInfo/MovieInfo';
+import Actors from './Actors/Actors';
 
 function App() {
   return (
@@ -10,9 +13,9 @@ function App() {
       <main>
         <Routes>
           <Route exact path="/" element={<Movies />} />
-          <Route exact path="/movie/:id" />
-          <Route exact path="/actors/:id" />
-          <Route exact path="/profile/:id" />
+          <Route exact path="/movie/:id" element={<MovieInfo />} />
+          <Route exact path="/actors/:id" element={<Actors />} />
+          <Route exact path="/profile/:id" element={<Profile />} />
         </Routes>
       </main>
     </div>
