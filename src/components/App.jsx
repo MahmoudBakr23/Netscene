93b7d5ad1c57f44369f133a3fd55a65f711c8cdf
Mahmoud Bakr <mbakr6821@gmail.com>
2022-09-12 11:47:19 +0200

    Create a source file to export components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { Route, Routes } from 'react-router-dom';
-import Movies from './Movies/Movies';
-import Profile from './Profile/Profile';
-import MovieInfo from './MovieInfo/MovieInfo';
-import Actors from './Actors/Actors';
+import { Movies, MovieInfo, Profile, Navbar, Actors } from './index';
 
 function App() {
   return (
     <div>
       <CssBaseline />
+      <Navbar />
       <main>
         <Routes>
           <Route exact path="/" element={<Movies />} />
