149d7b6fe6790f361fbac14ea23d8ed2f4e53c12
Mahmoud Bakr <mbakr6821@gmail.com>
2022-09-12 10:44:37 +0200

    Configuring the react routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { Route, Routes } from 'react-router-dom';
+import Movies from './Movies';
 
 function App() {
   return (
     <div>
       <CssBaseline />
-      <h1>Hello, World - Netscene</h1>
+      <main>
+        <Routes>
+          <Route exact path="/" element={<Movies />} />
+          <Route exact path="/movie/:id" />
+          <Route exact path="/actors/:id" />
+          <Route exact path="/profile/:id" />
+        </Routes>
+      </main>
     </div>
   );
 }
