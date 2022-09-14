30ad2e7bc3fd92ebe3123dabf733723ee7caeb39
Mahmoud Bakr <mbakr6821@gmail.com>
2022-09-14 02:04:04 +0200

    Finish navbar layout

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 import { AppBar, IconButton, Toolbar, Drawer, Button, Avatar, useMediaQuery } from '@material-ui/core';
 import { Menu, AccountCircle, Brightness4, Brightness7 } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
+import { useTheme } from '@material-ui/core/styles';
 
 import useStyles from './Navbar.styles';
 
 function Navbar() {
-  const classes = useStyles();
   const isMobile = useMediaQuery('(max-width:600px)');
+  const theme = useTheme();
+  const classes = useStyles({ theme });
+  const isAuthenticated = true;
   return (
     <AppBar position="fixed">
       <Toolbar className={classes.toolbar}>
         {isMobile && (
           <IconButton
-            color="inerit"
+            color="inherit"
             edge="start"
             style={{ outline: 'none' }}
             onClick={() => {}}
@@ -22,6 +25,33 @@ function Navbar() {
             <Menu />
           </IconButton>
         )}
+        <IconButton color="inherit" sx={{ ml: 1 }} onClick={() => {}}>
+          {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
+        </IconButton>
+        {!isMobile && 'Search...'}
+        <div>
+          {!isAuthenticated ? (
+            <Button color="inherit" onClick={() => {}}>
+              Login &nbsp; <AccountCircle />
+            </Button>
+          ) : (
+            <Button
+              color="inherit"
+              component={Link}
+              to="/profile/:id"
+              className={classes.linkButton}
+              onClick={() => {}}
+            >
+              {!isMobile && <>My Movies &nbsp;</>}
+              <Avatar
+                style={{ width: 30, height: 30 }}
+                alt="Profile"
+                src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
+              />
+            </Button>
+          )}
+        </div>
+        {isMobile && 'Search...'}
       </Toolbar>
     </AppBar>
   );
