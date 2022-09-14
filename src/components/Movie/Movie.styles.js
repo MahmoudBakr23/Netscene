f4900359b4e5251337f18217126dc00f128352be
Mahmoud Bakr <mbakr6821@gmail.com>
2022-09-14 10:41:04 +0200

    Style the Movie component

--- src/components/Movie/Movie.styles.js
+++ src/components/Movie/Movie.styles.js
@@ -4,6 +4,27 @@ const useStyles = makeStyles((theme) => ({
   movie: {
     padding: '10px',
   },
+  links: {
+    alignItems: 'center',
+    fontWeight: 'bolder',
+    textDecoration: 'none',
+    [theme.breakpoints.up('xs')]: {
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    '&:hover': {
+      cursor: 'pointer',
+      textDecoration: 'none',
+    },
+  },
+  image: {
+    borderRadius: '20px',
+    height: '300px',
+    marginBottom: '10px',
+    '&:hover': {
+      transform: 'scale(1.05)',
+    },
+  },
   title: {
     color: theme.palette.text.primaty,
     textOverflow: 'ellipsis',
